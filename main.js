5cd906f450c22e802730b08ad187ebd43cf0852f
Ram Ganesan Athreya <ramgnsn5@gmail.com>
2013-08-03 16:43:23 +0530

    Added compatibility for handling withheld grades

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $('document').ready(function() {
         if (/^[a-z][a-z][0-9]+/i.test(text)) {
             marks.subjects.push(text);
         }
-        else if (/^[a-z][.]?[a-z]?$/i.test(text)) {
+        else if (/^[a-z][.]?[a-z0-9]?$/i.test(text)) {
             marks.grades.push(text);
         }
     });
