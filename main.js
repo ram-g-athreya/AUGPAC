4cc365eceef54609898603e8057e4f2c43089c13
Ram Ganesan Athreya <ramgnsn5@gmail.com>
2013-08-03 16:41:36 +0530

    Extension modified to work with the latest version of anna university site

--- main.js
+++ main.js
@@ -1,45 +1,39 @@
-$('document').ready(function(){
-//Get 2nd row in table
-var tr=$('tr').next();
+$('document').ready(function() {
+    //All marks are within the strong tag
+    $('strong').each(function() {
+        var text = $(this).text();
+        //console.log(text);
 
-//If bold gets changed then this must be modified
-var headings=tr.find('b');
-var count=0;
+        if (/^[a-z][a-z][0-9]+/i.test(text)) {
+            marks.subjects.push(text);
+        }
+        else if (/^[a-z][.]?[a-z]?$/i.test(text)) {
+            marks.grades.push(text);
+        }
+    });
 
-
-marks.setHeadings(headings);
-
-$('tr').next().next().each(function(){
-	count=0;
-	$(this).find('center').each(function(){
-		var text=$.trim($(this).html());
-		if(count==marks.subject)marks.subjects.push(text);
-		else if(count==marks.grade)marks.grades.push(text);
-		else if(count==marks.result)marks.results.push(text);
-
-	count++;
-	});
-});
-
-marks.calculateGPA();
-if(!marks.invalid()){
-	marks.setValues();
-	chrome.storage.sync.set({'iframe': true});
-	$('body').append("<iframe src='" + chrome.extension.getURL('popup.html') + "' scrolling='yes'></iframe>");
-	check();
-}
+    //console.log("subjects are " + marks.subjects);
+    //console.log("grades are " + marks.grades);
+    
+    marks.calculateGPA();
+    if (!marks.invalid()) {
+        marks.setValues();
+        chrome.storage.sync.set({'iframe': true});
+        $('body').append("<iframe src='" + chrome.extension.getURL('popup.html') + "' scrolling='yes'></iframe>");
+        check();
+    }
 });
 
 //Function to check and remove the iframe
-function check(){
-	var close;
-	chrome.storage.sync.get(function(object){
-		close=object.close;
-		if(typeof close=='undefined'){
-			setTimeout(check, 500);
-		}
-		else {
-			$('iframe').remove();
-		}
-	});
+function check() {
+    var close;
+    chrome.storage.sync.get(function(object) {
+        close = object.close;
+        if (typeof close == 'undefined') {
+            setTimeout(check, 500);
+        }
+        else {
+            $('iframe').remove();
+        }
+    });
 }
